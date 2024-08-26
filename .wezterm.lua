7ab3191a21562e45ea5dd3e25d73ac3e99710875
Uzay Karadag <uzay@Uzays-MacBook-Pro.local>
2024-08-26 20:47:26 +0300

    Wezterm and zsh dotfiles.

---
+++ .wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Dracula+'
+
+-- Tilde fix
+config.send_composed_key_when_left_alt_is_pressed = true
+config.use_dead_keys = true
+
+-- Translucent window
+config.window_background_opacity = 0.3
+config.macos_window_background_blur = 20
+
+-- Natural jump word/line. 
+config.keys = {
+	{ mods = "OPT", key = "LeftArrow", action = wezterm.action.SendKey({ mods = "ALT", key = "b" }) },
+	{ mods = "OPT", key = "RightArrow", action = wezterm.action.SendKey({ mods = "ALT", key = "f" }) },
+	{ mods = "CMD", key = "LeftArrow", action = wezterm.action.SendKey({ mods = "CTRL", key = "a" }) },
+	{ mods = "CMD", key = "RightArrow", action = wezterm.action.SendKey({ mods = "CTRL", key = "e" }) },
+	{ mods = "CMD", key = "Backspace", action = wezterm.action.SendKey({ mods = "CTRL", key = "u" }) },
+}
+
+-- and finally, return the configuration to wezterm
+return config
+
